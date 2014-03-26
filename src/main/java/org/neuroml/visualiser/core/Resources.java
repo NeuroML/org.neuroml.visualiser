a8b13f864486d25706a8224a5981af5967e4142d
Padraig Gleeson <p.gleeson@gmail.com>
2014-03-26 15:35:53 +0000

    Minor changes in capitalisation of keywords

--- src/main/java/org/neuroml/visualiser/core/Resources.java
+++ src/main/java/org/neuroml/visualiser/core/Resources.java
@@ -8,20 +8,20 @@ package org.neuroml.visualiser.core;
 public enum Resources
 {
 	COND_DENSITY("Conductance density"),
-	SPIKE_THRESHOLD("Spike Threshold"),
-	REVERSAL_POTENTIAL("Reversal Potential"),
-	SPECIFIC_CAPACITANCE("Specific Capacitance"),
-	INIT_MEMBRANE_POTENTIAL("Initial Membrane Potential"),
+	SPIKE_THRESHOLD("Spike threshold"),
+	REVERSAL_POTENTIAL("Reversal potential"),
+	SPECIFIC_CAPACITANCE("Specific capacitance"),
+	INIT_MEMBRANE_POTENTIAL("Initial membrane potential"),
 	RESISTIVITY("Resistivity"),
-	MEMBRANE_P("Membrane Properties"),
-	INTRACELLULAR_P("Intracellular Properties"), 
+	MEMBRANE_P("Membrane properties"),
+	INTRACELLULAR_P("Intracellular properties"), 
 	SYNAPSE("Synapse Type"), 
-	CONNECTION_TYPE("Connection Type"),
+	CONNECTION_TYPE("Connection type"),
 	PRE_SYNAPTIC("Input"),
 	POST_SYNAPTIC("Output"), 
 	HIGHLIGHT("Highlight"), 
 	LOCATION("Location"), 
-	ION_CHANNELS("Ion Channels"), 
+	ION_CHANNELS("Ion channels"), 
 	HIGHLIGHT_CHAN_DENSITY("Highlight channel density");
 	private String _value;
 	
